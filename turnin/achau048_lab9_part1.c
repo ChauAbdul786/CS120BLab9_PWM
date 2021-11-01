7837aae093dfba20c2df1d70abad2cf92427c3cc
ChauAbdul786 <achau048@ucr.edu>
2021-10-31 23:13:42 -0700

    Added demo link for part 1

--- turnin/achau048_lab9_part1.c
+++ turnin/achau048_lab9_part1.c
@@ -6,138 +6,103 @@
  *
  *  I acknowledge all content contained herein, excluding template or example
  *  code, is my own original work.
+ *
+ *  Demo Link: https://drive.google.com/file/d/18ankF7LQiyj19twoE3AC2NgOSvL-hOxp/view?usp=sharing
+ *
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
+#include "timer.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-volatile unsigned char TimerFlag = 0;
+void set_PWM(double frequency){
+    static double currFreq;
+    if(frequency != currFreq){
+        if(!frequency) { 
+            TCCR3B |= 0x08;
+        }else{
+            TCCR3B |= 0x03;
+        }
 
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
+        if(frequency < .954){
+            OCR3A = 0xFFFF;
+        }else if(frequency > 31250){
+            OCR3A = 0x0000;
+        }else{
+            OCR3A = (short)(8000000 / (128 * frequency)) - 1;
+        }
 
-void TimerOn() {
-
-    TCCR1B = 0x0B;
-    OCR1A = 125;
-    TIMSK1 = 0x02;
-    TCNT1 = 0;
-    _avr_timer_cntcurr = _avr_timer_M;
-    SREG |= 0x80;
-}
-
-void TimerOff() {
-    TCCR1B = 0x00;
-}
-
-void TimerISR() {
-    TimerFlag = 1;
-}
-
-ISR(TIMER1_COMPA_vect) {
-    _avr_timer_cntcurr--;
-    if (_avr_timer_cntcurr == 0) {
-        TimerISR();
-        _avr_timer_cntcurr = _avr_timer_M;
-    }
-}
-
-void TimerSet(unsigned long M) {
-    _avr_timer_M = M;
-    _avr_timer_cntcurr = _avr_timer_M;
-}
-
-void set_PWM(double frequency) {
-    static double current_frequency;
-    if (frequency != current_frequency) {
-        if (!frequency) { TCCR3B &= 0x08; }
-        else { TCCR3B |= 0x03; }
-        if (frequency < 0.954) { OCR3A = 0xFFFF; }
-        else if (frequency > 31250) { OCR3A = 0x0000; }
-        else { OCR3A = (short) (8000000 / (128 * frequency)) -1; }
         TCNT3 = 0;
-        current_frequency = frequency;
+        currFreq = frequency;
     }
 }
 
-void PWM_on() {
+void PWM_on(){
     TCCR3A = (1 << COM3A0);
     TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
     set_PWM(0);
 }
 
-void PWM_off() {
+void PWM_off(){
     TCCR3A = 0x00;
     TCCR3B = 0x00;
 }
 
-enum SM_STATES {SM1_SMStart, SM_FirstButton, SM_SecondButton, SM_ThirdButton} SM_STATE;
+enum States {Start, Button1, Button2, Button3} state;
 
-unsigned char whichLight = 0;
-unsigned char afterThree = 0;
 
-void Tick_LoHi() {
-    switch(SM_STATE) {
-       case SM1_SMStart:
-          if ((~PINA & 0x04) && !(~PINA & 0x02) && !(~PINA & 0x01))
-          {
-             SM_STATE =  SM_ThirdButton;
-          }
-          else if ((~PINA & 0x02) && !(~PINA & 0x04) && !(~PINA & 0x01))
-          {
-             SM_STATE = SM_SecondButton;
-          }
-          else if ((~PINA & 0x01) && !(~PINA & 0x04) && !(~PINA & 0x02))
-          {
-             SM_STATE = SM_FirstButton;
-          }
-          else
-          {
-         SM_STATE = SM1_SMStart;
+void Tick() {
+    switch(state) {
+       case Start:
+          if(PINA & 0x07 == 0x01){
+             state =  Button1;
+          }else if(PINA & 0x07 == 0x02){
+             state = Button2;
+          }else if(PINA & 0x07 == 0x04){
+             state = Button3;
+          }else{
+            state = Start;
           }
           break;
-       case SM_FirstButton:
-          if ((~PINA & 0x01) && !(~PINA & 0x04) && !(~PINA & 0x02)) {
-         SM_STATE = SM_FirstButton;
-          }
-          else {
-             SM_STATE = SM1_SMStart;
-          }
-          break;
-       case SM_SecondButton:
-          if ((~PINA & 0x02) && !(~PINA & 0x04) && !(~PINA & 0x01)) {
-         SM_STATE = SM_SecondButton;
-          }
-          else {
-             SM_STATE = SM1_SMStart;
+
+       case Button1:
+          if(PINA & 0x07 == 0x01) {
+            state = Button1;
+          }else{
+            state = Start;
           }
           break;
-       case SM_ThirdButton:
-          if ((~PINA & 0x04) && !(~PINA & 0x02) && !(~PINA & 0x01)) {
-         SM_STATE = SM_ThirdButton;
+       case Button2:
+          if(PINA & 0x07 == 0x02) {
+            state = Button2;
+          }else{
+            state = Start;
           }
-          else {
-             SM_STATE = SM1_SMStart;
+          break;
+       case Button3:
+          if (PINA & 0x07 == 0x04) {
+            state = Button3;
+          }else{
+            state = Start;
           }
           break;
        default:
-          SM_STATE = SM1_SMStart;
+          state = Start;
           break;
     }
 
-    switch(SM_STATE) {
-       case SM1_SMStart:
+    switch(state) {
+       case Start:
           set_PWM(0);
           break;
-       case SM_FirstButton:
+       case Button1:
           set_PWM(261.63);
           break;
-       case SM_SecondButton:
+       case Button2:
           set_PWM(293.66);
           break;
-       case SM_ThirdButton:
+       case Button3:
           set_PWM(329.63);
           break;
        default:
@@ -145,21 +110,22 @@ void Tick_LoHi() {
     }
 }
 
-void main() {
 
+int main(void) {
+    /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
 
+    /* Insert your solution below */
     TimerSet(200);
     TimerOn();
-    
     PWM_on();
-
-    SM_STATE = SM1_SMStart;
+    state = Start;
 
     while(1) {
-        Tick_LoHi();
+        Tick();
         while (!TimerFlag);
         TimerFlag = 0;
     }
+    return 1;
 }
